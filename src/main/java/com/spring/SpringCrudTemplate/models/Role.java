71933c24f9304ebc47a5cd62314fa6db33343318
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-02 19:21:36 +0300

    Added Role class

---
+++ src/main/java/com/spring/SpringCrudTemplate/models/Role.java
@@ -0,0 +1,17 @@
+package com.spring.SpringCrudTemplate.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+}
