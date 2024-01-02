b2b1a4189eb9db47aad25e9cad1ce0a9dbd25be4
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-02 19:08:49 +0300

    Added AppUserRepository

---
+++ src/main/java/com/spring/SpringCrudTemplate/repositories/AppUserRepository.java
@@ -0,0 +1,16 @@
+package com.spring.SpringCrudTemplate.repositories;
+
+import com.spring.SpringCrudTemplate.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    Optional<AppUser> findByEmail(String email);
+
+}
