b7417ddf05d37bf4c982344fabd085f4e1f5a42f
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-02 20:10:11 +0300

    Added RoleRepository

---
+++ src/main/java/com/spring/SpringCrudTemplate/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.spring.SpringCrudTemplate.repositories;
+
+import com.spring.SpringCrudTemplate.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
