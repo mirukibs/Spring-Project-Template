14191d69ea19de695bbb21a6833b69d10b54aa45
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-02 14:37:08 +0300

    Initial commit

---
+++ src/test/java/com/spring/SpringCrudTemplate/SpringCrudTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringCrudTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
