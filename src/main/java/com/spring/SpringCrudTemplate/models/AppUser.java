5470e09683c7313ac59111dfead97c3b5697204e
Kibwana Miruru <99078034+mirukibs@users.noreply.github.com>
2024-01-02 19:59:18 +0300

    Added AppUserService and modified AppUser class

--- src/main/java/com/spring/SpringCrudTemplate/models/AppUser.java
+++ src/main/java/com/spring/SpringCrudTemplate/models/AppUser.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class AppUser implements UserDetails {
+public class AppUser {
 
     @SequenceGenerator(
             name = "user_sequence",
@@ -43,36 +43,8 @@ public class AppUser implements UserDetails {
     private List<Role> roles = new ArrayList<>();
 
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
     public String getUsername() {
         return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
     }
 
 }
---
+++ src/main/java/com/spring/SpringCrudTemplate/services/AppUserService.java
@@ -0,0 +1,57 @@
+package com.spring.SpringCrudTemplate.services;
+
+import com.spring.SpringCrudTemplate.models.AppUser;
+import com.spring.SpringCrudTemplate.models.Role;
+import com.spring.SpringCrudTemplate.repositories.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+    private final static String USER_NOT_FOUND_MSG = "user with email %s not found";
+
+    private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+
+
+        return new User(appUser.getUsername(), appUser.getPassword(), mapRolesToAuthorities(appUser.getRoles()));
+    }
+
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    public String signUpUser(AppUser appUser) {
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+
+        if (userExists) {
+            throw new IllegalStateException("Email already taken");
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+
+        appUser.setPassword(encodedPassword);
+
+        appUserRepository.save(appUser);
+
+        return "Congratulation! You have been registered successfully.";
+    }
+
+}
